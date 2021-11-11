900d2aa51e040cc37afebc9db9989733e63f14bd
Johan Meneses <johanmeci03@gmail.com>
2021-11-11 17:10:46 -0500

    Adding course to cart list

---
+++ js/app.js
@@ -0,0 +1,77 @@
+//Variables
+const cart = document.querySelector('#div-cart');
+const containerList = document.querySelector('#cart-list tbody');
+const btnEmpty = document.querySelector('#empty-cart');
+const courseList = document.querySelector('#course-list');
+
+let cartList = [];
+
+loadEventListeners();
+
+function loadEventListeners() {
+    courseList.addEventListener('click', addItem);
+}
+
+//Functions
+function addItem(e) {
+
+    e.preventDefault();
+
+    if (e.target.classList.contains('add-item')) {
+        const selectedCourse = e.target.parentElement.parentElement;
+        getCourse(selectedCourse);
+    }
+
+}
+
+function getCourse(course) {
+    
+    const dataCourse = {
+        id: course.querySelector('a').getAttribute('data-id'),
+        title: course.querySelector('h4').textContent,
+        price: course.querySelector('.price span').textContent,
+        img: course.querySelector('img').src,
+        qty: 1
+    }
+
+    //Add elements to cartList array
+    cartList = [...cartList, dataCourse];
+
+    addCartListHTML();
+}
+
+//Add elements in cartList HTML container
+function addCartListHTML() {
+
+    cleanContainerList();
+
+    cartList.forEach(course => {
+
+        const {img, title, price, qty, id} = course;
+        const row = document.createElement('tr');
+        row.innerHTML = `
+                        <td>
+                            <img class='img-td' src='${img}' />
+                        </td>
+                        <td>${title}</td>
+                        <td>${price}</td>
+                        <td>${qty}</td>
+                        <td>
+                            <a class='remove-item' data-id='${id}' href='#'> X </a>
+                        </td>
+                        `;
+
+        containerList.appendChild(row);
+
+    });
+    
+}
+
+
+function cleanContainerList() {
+    
+    while (containerList.firstChild) {
+        containerList.removeChild(containerList.firstChild)
+    }
+
+}
